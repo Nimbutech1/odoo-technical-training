74c744f92166a5dd2fbf06a75b9302fb1cb2dda1
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-01 13:13:12 +0000

    [ADD] act_window actions and security

--- cooperative_volunteers/__manifest__.py
+++ cooperative_volunteers/__manifest__.py
@@ -25,7 +25,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/cooperative_volunteers_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
