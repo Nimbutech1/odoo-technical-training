a1e3e920154ad490749a3a7a10d01f6e71732158
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-06 17:55:38 +0000

    [CHANGE] default image

--- custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
+++ custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,22 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo import tools
+from odoo.modules import get_module_resource
+import base64
 
 class Partnet(models.Model):
     _inherit = 'res.partner'
     
-    image = fields.Binary(string="Image", attachment=True)
+    image = fields.Binary("Photo", default='_get_default_image')
     
-    def default_image(self, context=None):
-        return 'static/img/image.jpg'
-    
-    # Set the default image for res_partner
-    res_partner_default_image = fields.Binary('Default Image', attachment=True, default=lambda self: self._get_default_image())
-
     def _get_default_image(self):
-        image_path = 'path/to/default/image.png'
-        return open(image_path, 'rb').read()
-
+        image_path = modules.get_module_resource('custom_modules', 'static/src/img', 'image.jpg')
+        return tools.image_resize_image_big(base64.b64encode(open(image_path, 'rb').read()))
 
 # class custom_modules(models.Model):
 #     _name = 'custom_modules.custom_modules'
