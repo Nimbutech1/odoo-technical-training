68baf23b8d3fb8d57e6c39baab9b0870c62e050c
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-06 17:38:57 +0000

    [REPLACE] image partner

--- custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
+++ custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
@@ -5,8 +5,17 @@ from odoo import models, fields, api
 class Partnet(models.Model):
     _inherit = 'res.partner'
     
+    image = fields.Binary(string="Image", attachment=True)
+    
     def default_image(self, context=None):
         return 'static/img/image.jpg'
+    
+    # Set the default image for res_partner
+    res_partner_default_image = fields.Binary('Default Image', attachment=True, default=lambda self: self._get_default_image())
+
+    def _get_default_image(self):
+        image_path = 'path/to/default/image.png'
+        return open(image_path, 'rb').read()
 
 
 # class custom_modules(models.Model):
