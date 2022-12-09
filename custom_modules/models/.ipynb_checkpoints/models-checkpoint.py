45b594d2b1cd7a4362ba79adc16d27e5486166c2
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-09 17:14:25 +0000

    [ADD] custom modules

--- custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
+++ custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
@@ -6,12 +6,21 @@ from odoo.exceptions import UserError, ValidationError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     
-    @api.model
-    def create(self, vals_list):
-        res = super(ResPartner, self).create(vals_list)
-        raise Usererrpr("Error")
-        return res
-        
+    def _avatar_get_placeholder_path(self):
+        if self.is_company:
+            return "custom_modules/static/img/custom_company.png"
+        if self.type == 'delivery':
+            return "base/static/img/truck.png"
+        if self.type == 'invoice':
+            return "base/static/img/money.png"
+        return super()._avatar_get_placeholder_path()
+    
+class AvatarMixin(models.AbstractModel):
+    _inherit = avatar.mixin
+    
+    def _avatar_get_placeholder_path(self):
+        return "custom_modules/static/img/custom_user.png"
+    
 # class custom_modules(models.Model):
 #     _name = 'custom_modules.custom_modules'
 #     _description = 'custom_modules.custom_modules'
